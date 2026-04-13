a05ae6ddba374892965693b8884f4ad140b84762
Badr Morsadi <bmorsadi@gmail.com>
2026-04-13 05:23:03 -0700

    scaffold app with Tailwind and Supabase deps

---
+++ dashboard/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Home() {
+	return (
+		<div className='h-screen flex items-center justify-center bg-red'>
+			<h1 className='text-5xl text-400 font-bold'>Dashboard</h1>
+		</div>
+	);
+}
